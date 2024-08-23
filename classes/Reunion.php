6950b5bfed7e5c1c47d4f16a1840ef2ed63db862
Pedro Roberti <pedroroberti99@gmail.com>
2024-08-23 12:48:20 -0300

    completamos clases reunion y solicitud

--- classes/Reunion.php
+++ classes/Reunion.php
@@ -1,3 +1,4 @@
+
 <?php
 class Reunion
 {
@@ -9,4 +10,54 @@ class Reunion
     // Descripción opcional
 
     // TODO: Getters y setters
+
+    //Tema
+    protected $tema;
+    //Fecha(Clave Primaria)
+    protected $fecha;
+    //HoraInicio(Clave Primaria)
+    protected $horaInicio;
+    //HoraFin(Clave primaria)
+    protected $horaFin
+    //Descripcion
+    protected $descripcion
+
+
+    public function __construct($tema, $fecha, $horaInicio, $horaFin, $descripcion)
+    {
+        $this->tema = $tema;
+        $this->fecha = $fecha;
+        $this->horaInicio = $horaInicio;
+        $this->horaFin = $horaFin;
+        $this->descripcion = $descripcion;
+    }
+
+
+    public function GetTema()
+    {
+        return "$this->tema";
+    }
+    public function getFecha()
+    {
+        return $this->nombre;
+    }
+    public function getHoraInicio()
+    {
+        return $this->apellido;
+    }
+    public function getHoraFin()
+    {
+        return $this->email;
+    }
+    public function getDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+    }
+
+
+    
 }
+
+
+
+
--- classes/Solicitud.php
+++ classes/Solicitud.php
@@ -1,6 +1,15 @@
 <?php
 class Solicitud
 {
+    //TipoSolicitud(clave primeria)
+    protected $TipoSolicitud;
+    //Dni(solicitante clave primaria/secundaria)
+    protected $DniSolicitante;
+    //FechaDesde
+    protected $FechaDesde;
+    //FechaHasta
+    protected $FechaHasta;
+
     // TODO: Atributos:
     // Tipo de Solicitud (Clave primaria)
     // Fecha de la Solicitud (Clave primaria)
@@ -8,5 +17,33 @@ class Solicitud
     // Fecha desde cuando inicia la solicitud
     // Fecha de hasta cuando es de la solicitud
 
+    public function __construct($TipoSolicitud, $DniSolicitante, $FechaDesde ,$FechaHasta)
+    {
+        $this->TipoSolicitud = $TipoSolicitud;
+        $this->DniSolicitante = $DniSolicitante;
+        $this->FechaDesde = $FechaDesde;
+        $this->FechaHasta = $FechaHasta;
+
+    }
+
     // TODO: Getters y setters
+    public function getTipoSolicitud()
+    {
+        return "$this->TipoSolicitud";
+    }
+    public function getDniSolicitante()
+    {
+        return $this->DniSolicitante;
+    }
+    public function getFechaDesde()
+    {
+        return $this->FechaDesde;
+    }
+    public function getFechaHasta()
+    {
+        return $this->FechaHasta;
+    }
+
+
+
 }
